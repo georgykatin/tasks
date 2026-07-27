0c3263b7edeaec13ae142aeece1e41ba23b2e93b
Mikhail Mikryukov <mikryukovm.a@yandex.ru>
2026-07-27 16:07:08 +0300

    задачи по конкарренси

--- concurrency/task-09/main.go
+++ concurrency/task-09/main.go
@@ -3,28 +3,57 @@
 // 2) Нужен ли тут контекст, если да, то какой?
 // 3) Как поправить код, чтобы не бояться зависания воркеров ?
 // 4) Где должен проверяться `<-ctx.Done()` ?
+
+// 1) горутина зависнет, значит будет утечка памяти. закрытие не поможет потому что воркер зависнет уже в блоке for range
+// 2) нужен. WithCancel для ручной отмены WithTimeout/WithDeadline для отмены по времени
+// 3) добавить селект с проверкой завершения контекста
+// 4) должен проверяться в момент отправки джобы, внутри воркера в момент взятия джобы и в момент обработки джобы (на случай если долгая операция)
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 )
 
-func worker(jobs <-chan int) {
-	for job := range jobs {
-		fmt.Println("processing", job)
-		time.Sleep(2 * time.Second)
+func worker(ctx context.Context, jobs <-chan int) {
+	for {
+		select {
+		case job, ok := <-jobs:
+			if !ok {
+				return
+			}
+
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(2 * time.Second):
+				fmt.Printf("job %v one", job)
+			}
+		case <-ctx.Done():
+			return
+		}
 	}
+	//for job := range jobs {
+	//	fmt.Println("processing", job)
+	//	time.Sleep(2 * time.Second)
+	//}
 }
 
 func main() {
 	jobs := make(chan int)
-
-	go worker(jobs)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	for i := 0; i < 5; i++ {
-		jobs <- i
+		select {
+		case jobs <- i:
+			go worker(ctx, jobs)
+		case <-ctx.Done():
+			break
+		}
+
+		//jobs <- i
 	}
 
-	
 }
